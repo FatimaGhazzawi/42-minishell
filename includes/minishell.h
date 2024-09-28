59eaeda81b82bcb69cc02bfc5c4d3221a46f36ce
Hady <hady_awayda@hotmail.com>
2024-09-28 20:56:34 +0300

    Added dummy AST syntax

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hawayda <hawayda@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 23:09:17 by fel-ghaz          #+#    #+#             */
-/*   Updated: 2024/09/27 05:31:54 by hawayda          ###   ########.fr       */
+/*   Updated: 2024/09/28 20:12:00 by hawayda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,45 @@ typedef struct s_env
 	char			*value;
 	struct s_env	*next;
 }					t_env;
+
+typedef enum e_ast_type {
+    AST_COMMAND,
+    AST_PIPE,
+    AST_REDIRECTION
+} t_ast_type;
+
+// Enum to define types of redirections
+typedef enum e_redirection_type {
+    REDIR_INPUT,      // <
+    REDIR_OUTPUT,     // >
+    REDIR_APPEND,     // >>
+    REDIR_HEREDOC     // <<
+} t_redirection_type;
+
+// Structure to define a command node
+typedef struct s_command {
+    char **argv;          // Command and arguments
+    char *input_file;     // Input file for redirection
+    char *output_file;    // Output file for redirection
+    t_redirection_type output_type; // Type of output redirection
+} t_command;
+
+// Structure to define an AST node
+typedef struct s_ast_node {
+    t_ast_type type;              // Type of the node (command, pipe, etc.)
+    union {
+        t_command *command;       // Command node
+        struct {
+            struct s_ast_node *left;  // Left child (e.g., the command before the pipe)
+            struct s_ast_node *right; // Right child (e.g., the command after the pipe)
+        } pipe;
+        struct {
+            struct s_ast_node *command_node; // Command to redirect
+            t_redirection_type type;         // Type of redirection
+            char *file;                      // File to redirect to
+        } redirection;
+    };
+} t_ast_node;
 
 // typedef struct nodee
 // {
